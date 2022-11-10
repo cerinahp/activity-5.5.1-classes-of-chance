db3163cdbc129041e0ab412774310f194025718c
cerina Malaluan <cerinamalaluan@gmail.com>
2022-11-09 20:36:01 -0800

    first

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 class Casino {
     // Write code here
-};
+
+    constructor(name,) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.fakeCoin = cheater;
+
+
+    }
+    playGame(num) {
+        let randomNum = Math.random() 
+        if (this.fakeCoin or randomNum < .5) {
+            console.log(`${this.name} wins`)
+        } else { 
+            let winnings = bet * this.timesPlayed + 1
+            console.log(`you win $${winnings}`)
+
+        }
+        }
+        this.timesPlayed += 1;
+
+
+    };
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
@@ -11,20 +32,20 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
